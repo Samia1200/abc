f0d94c3ff1f0055590e0014f163da396427480fc
Samia1200 <135737545+Samia1200@users.noreply.github.com>
2023-06-07 11:43:53 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,17 @@
+count=0 #start
+while(count<3): #condition
+    print("the count is:",count)
+    count=count+1 #end
+print("ar pari na")
+
+#count=0 #start
+#if(count<3): #condition
+    #print("the count is:",count)
+    #count=count+1 #end
+#print("ar pari na")
+
+n=0
+while(n<11): #condition
+    print("the natural number is",n)
+    n=n+1 #end
+print("End")
